9e4a83d63eea5798e0183e1f6d6baae00f81ff1a
Akaza <asustufa15xx@gmail.com>
2026-06-20 15:09:37 +0300

    delete unnecessary files

--- test.sh
+++
@@ -1,5 +0,0 @@
-wget https://www.manageengine.com/network-monitoring/29809517/ManageEngine_OpManager_64bit.bin
-
-sudo chmod +x ManageEngine_OpManager_64bit.bin
-
-sudo ./ManageEngine_OpManager_64bit.bin -i console
